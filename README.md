6561d55ee43485c0c85ff5e96205b35e27933566
deadManAlive <alfarrakhan@gmail.com>
2023-02-21 00:23:27 +0700

    add vector to contain file paths

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Windows 10's Photos App Live Tile Changer ported to Rust.
 Example of `config.toml` (place into working directory):
 ```toml
 [config]
-debug = true
+debug = true # optional, default to false
 locations = [
     "C:/Users/root/Pictures/",
     "D:/images",
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use std::io::Error;
-
-use walkdir::WalkDir;
+use std::{io::Error, time::Instant};
+use walkdir::{DirEntry, WalkDir};
 
 mod config;
 
@@ -9,22 +8,30 @@ const IMG_EXTENSIONS: [&str; 3] = [".jpg", ".jpeg", ".png"];
 fn main() -> Result<(), Error> {
     let cfg = config::Config::read()?;
 
-    let mut count: u32 = 0;
+    let mut flist: Vec<DirEntry> = vec![];
+
+    let start = Instant::now();
 
     for dir in cfg.locations {
-        for entry in WalkDir::new(dir).into_iter().filter_map(|e| e.ok()) {
-            let f_name = entry.path().to_string_lossy();
+        let mut files: Vec<DirEntry> = WalkDir::new(&dir)
+            .into_iter()
+            .filter_map(|e| e.ok())
+            .filter_map(|f| -> Option<DirEntry> {
+                let f_name = f.file_name().to_string_lossy();
 
-            for extension in IMG_EXTENSIONS {
-                if f_name.ends_with(extension) {
-                    println!("{}", f_name);
-                    count += 1;
+                for extension in IMG_EXTENSIONS {
+                    if f_name.ends_with(extension) {
+                        return Some(f);
+                    }
                 }
-            }
-        }
+                None
+            })
+            .collect();
+
+        flist.append(&mut files);
     }
 
-    println!("found {} files", count);
+    println!("found {} files in {} ms.", flist.len(), start.elapsed().as_millis());
 
     Ok(())
 }
